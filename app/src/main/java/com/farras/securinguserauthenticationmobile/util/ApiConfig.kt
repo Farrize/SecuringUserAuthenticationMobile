61e9395260b35805cbb98b81f2ea88117e76def7
Farrize <strikeit3214@gmail.com>
2023-04-08 20:33:49 +0700

    Adding config for API connections

---
+++ app/src/main/java/com/farras/securinguserauthenticationmobile/util/ApiConfig.kt
@@ -0,0 +1,9 @@
+package com.farras.securinguserauthenticationmobile.util
+
+class ApiConfig {
+    companion object {
+        const val BASE_URL = "127.0.0.1:3000"
+        const val LOGIN_URL = "/login"
+        const val REGISTER_URL = "/register"
+    }
+}
